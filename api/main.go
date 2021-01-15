b4e4eb899a824180d45e1838e6e241e6835ea068
my-Sakura <69021934+my-Sakura@users.noreply.github.com>
2021-01-16 03:06:07 +0800

    test

---
+++ api/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+type result struct {
+	From         string `json: "from"`
+	To           string `json: "to"`
+	Trans_result []test `json: "trans_result"`
+}
+
+type test struct {
+	Src string `json: "src"`
+	Dst string `json: "dst"`
+}
+
+func httpGet(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+var bot *tgbotapi.BotAPI
+
+func init() {
+	//new telegramBot
+	token := os.Getenv("token")
+
+	bot.Debug = true
+	bot, _ = tgbotapi.NewBotAPI(token)
+	//new update
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	link := "https://telegram-translate-bot.vercel.app/"
+	bot.SetWebhook(tgbotapi.NewWebhook(link + token))
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	var update tgbotapi.Update
+	if update.Message == nil {
+		return
+	}
+
+	body, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(body, &update)
+
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+	var target result
+	//baidu appid
+	appid := os.Getenv("appid")
+	q := update.Message.Text
+	salt := 123
+	secret := os.Getenv("secret")
+	data := []byte(appid + q + strconv.Itoa(salt) + secret)
+	sign := fmt.Sprintf("%x", md5.Sum(data))
+	url := fmt.Sprintf("https://fanyi-api.baidu.com/api/trans/vip/translate?q=%s&from=%s&to=%s&salt=%d&appid=%s&sign=%s", q, "auto", "en", salt, appid, sign)
+	result, err := httpGet(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = json.Unmarshal(result, &target)
+	if err != nil {
+		fmt.Println(err)
+	}
+	msg.Text = target.Trans_result[0].Dst
+	if _, err = bot.Send(msg); err != nil {
+		fmt.Println(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
-module test
+module github.com/my-Sakura/telegram_translate_bot
 
 go 1.15
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+)
